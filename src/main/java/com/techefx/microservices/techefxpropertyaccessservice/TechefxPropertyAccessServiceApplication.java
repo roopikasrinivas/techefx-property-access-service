d26b6e03256592bebd050f11c47add0a01d612ca
Roopika Srinivas <13376391+roopikasrinivas@users.noreply.github.com>
2022-04-19 10:51:11 +0530

    Initial commit

---
+++ src/main/java/com/techefx/microservices/techefxpropertyaccessservice/TechefxPropertyAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techefx.microservices.techefxpropertyaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechefxPropertyAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechefxPropertyAccessServiceApplication.class, args);
+    }
+
+}
